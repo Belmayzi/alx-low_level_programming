5466475b3e12ce3082745ea4492cbaa8e15799a2
Belmayzi <belmayziyassir@gmail.com>
2023-06-19 14:04:41 -0700

    talha

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nbr;
+
+	for (nbr = '0' ; nbr <= '9' ; nbr++)
+	{
+		putchar(nbr);
+	}
+		putchar('\n');
+
+	return (0);
+}
