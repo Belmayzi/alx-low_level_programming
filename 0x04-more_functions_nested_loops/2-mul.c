7ad8d9539b6cf144ddbd8dcc97dc2e16c16a1978
Belmayzi <belmayziyassir@gmail.com>
2023-06-23 04:33:08 -0700

    correction

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,10 +4,9 @@
  * mul - function that multiplies two integers.
  * @a: first integer
  * @b: second integer
- * Return: Multiplication of a and b 
+ * Return: Multiplication of a and b
  */
 int mul(int a, int b)
-
 {
 	return(a * b);
 }
