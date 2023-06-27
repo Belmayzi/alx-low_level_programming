d4b780ba2214fe7cf9f17e44d6fc1e5e20b4351f
Belmayzi <belmayziyassir@gmail.com>
2023-06-27 06:08:53 -0700

    first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
 * reset_to_98 - takes a pointer as an input and updated its value to 98.
-*@n: int to check. 
+*@n: int to check.
 *
 *Return: 0 is success.
-*/ 
+*/
 
 void reset_to_98(int *n)
 {
